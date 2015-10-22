7fb863bfb19e6d3c8c078997166b7ca53155326b
Jiri Horner <laeqten@gmail.com>
2015-10-22 12:55:04 +0200

    format in solaris c-style
    same formatting used through course

--- wc/wc.c
+++ wc/wc.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <string.h>
 
-#define init_size 10
+#define	init_size 10
 
 typedef struct {
 	size_t count;
@@ -12,7 +12,7 @@ typedef struct {
 static Counter *counter_arr;
 static size_t counter_arr_size = init_size;
 
-static void add_word(char* word) {
+static void add_word(char *word) {
 	for (Counter* it = counter_arr; it < counter_arr + counter_arr_size; ++it) {
 		if (it->word == NULL) {
 			it->word = word;
@@ -26,8 +26,8 @@ static void add_word(char* word) {
 		}
 	}
 	// array is too small
-	counter_arr = realloc(counter_arr, sizeof(Counter)*counter_arr_size*2); // double array size
-	memset(counter_arr + counter_arr_size, 0, sizeof(Counter)*counter_arr_size); // zero new part of array
+	counter_arr = realloc(counter_arr, sizeof (Counter)*counter_arr_size*2); // double array size
+	memset(counter_arr + counter_arr_size, 0, sizeof (Counter)*counter_arr_size); // zero new part of array
 	counter_arr_size *= 2;
 	// try again with larger array
 	add_word(word);
@@ -35,18 +35,18 @@ static void add_word(char* word) {
 
 int main(int argc, char const *argv[]) {
 	// allocate array for counting
-	counter_arr = calloc(init_size, sizeof(Counter));
-	
+	counter_arr = calloc(init_size, sizeof (Counter));
+
 	char *word = NULL;
-	while(scanf("%ms", &word) > 0) {
+	while (scanf("%ms", &word) > 0) {
 		// %m is posix 2008 extension od ISO C
 		add_word(word);
 	}
 	for (Counter* it = counter_arr; it < counter_arr + counter_arr_size; ++it) {
 		if (it->word == NULL)
-			return 0;
+			return (0);
 
 		printf("%s %lu\n", it->word, it->count);
 	}
-	return 0;
+	return (0);
 }
