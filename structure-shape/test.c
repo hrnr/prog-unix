32a0c9ae9886f0607834e03e4cc4a3fcdfaf282e
Jiri Horner <laeqten@gmail.com>
2016-03-10 12:38:32 +0100

    structure_shape: add first test
    tests default value handling

--- structure-shape/test.c
+++ structure-shape/test.c
@@ -5,13 +5,34 @@
 
 #include "struct.h"
 
-/* A test case that does nothing and succeeds. */
+/* invariant valid for entry structure */
+#define entry_assert(e) \
+	assert_true(e.timer_soft_sec < e.timer_hard_sec); \
+	assert_true(e.timer_soft_kb < e.timer_hard_kb); \
+	assert_true(e.timer_hard_sec - e.timer_soft_sec >= MINDIFF); \
+	assert_true(e.timer_hard_kb - e.timer_soft_kb >= MINDIFF);
+
+#define LENGTH(x) (sizeof x / sizeof x[0])
+
+/* tests */
+
+// a test case that does nothing and succeeds (tests framework)
 static void null_test_success(void **state) {
     (void) state; /* unused */
 }
+
+// test default value handling
+static void entry_default(void **state) {
+	char *argv[] = { "shape",  "0", "0", "0", "0"};
+
+	entry_t e = shape(LENGTH(argv), argv);
+	entry_assert(e);
+}
+
 int main(void) {
     const struct CMUnitTest tests[] = {
         cmocka_unit_test(null_test_success),
+        cmocka_unit_test(entry_default),
     };
     return cmocka_run_group_tests(tests, NULL, NULL);
 }
